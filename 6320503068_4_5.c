aed27ddfe96d3cdc796166dcaecfd5ab396b868f
Piya <piya.rat@ku.th>
2021-03-04 17:32:35 +0700

    13

--- 6320503068_4_5.c
+++ 6320503068_4_5.c
@@ -2,55 +2,56 @@
 #include<string.h>
 #include<ctype.h>
     void main(){
-        int n,i,j;
-        char word[50],cup[4]={"A"},token[50];
+        int n,i,j,cup[4]={1},token;
+        char word[50];
         scanf("%d",&n);
         if(n>0&&n<100){
             for( i=0 ; i<n ; i++ ){
+                scanf("%s",word);
                 if(strlen(word)<=50){
-                    scanf("%s",word);
+                    for( j=0 ; j<strlen(word) ; j++ ){
+                        if(strcmp(word[j],'A')){
+                            token=cup[2];
+                            cup[2]=cup[1];
+                            cup[1]=token;
+                        }
+                        else if(strcmp(word[j],'B')){
+                            token=cup[4];
+                            cup[4]=cup[1];
+                            cup[1]=token;
+                        }
+                        else if(strcmp(word[j],'C')){
+                            token=cup[3];
+                            cup[3]=cup[1];
+                            cup[1]=token;
+                            token=cup[2];
+                            cup[2]=cup[4];
+                            cup[4]=token;
+                        }
+                        else if(strcmp(word[j],'D')){
+                            token=cup[2];
+                            cup[2]=cup[1];
+                            cup[1]=token;
+                            token=cup[3];
+                            cup[3]=cup[4];
+                            cup[4]=token;
+                        }
+                        else if(strcmp(word[j],'E')){
+                            token=cup[4];
+                            cup[4]=cup[1];
+                            cup[1]=token;
+                            token=cup[2];
+                            cup[2]=cup[3];
+                            cup[3]=token;
+                        }
+                    }
+                    for( j=0 ; j<4 ; j++ ){
+                        if(cup[j]==1){
+                            printf("%d",j+1);
+                        }
+                    }
                 }
-                for( j=0 ; j<strlen(word) ; j++ ){
-                    if(strcmp(word[j],"A")){
-                        strcpy(cup[2],token);
-                        strcpy(cup[1],cup[2]);
-                        strcpy(token,cup[1]);
-                    }
-                    else if(strcmp(word[j],"B")){
-                        strcpy(cup[4],token);
-                        strcpy(cup[1],cup[4]);
-                        strcpy(token,cup[1]);
-                    }
-                    else if(strcmp(word[j],"C")){
-                        strcpy(cup[3],token);
-                        strcpy(cup[1],cup[3]);
-                        strcpy(token,cup[1]);
-                        strcpy(cup[4],token);
-                        strcpy(cup[2],cup[4]);
-                        strcpy(token,cup[2]);
-                    }
-                    else if(strcmp(word[j],"D")){
-                        strcpy(cup[2],token);
-                        strcpy(cup[1],cup[2]);
-                        strcpy(token,cup[1]);
-                        strcpy(cup[3],token);
-                        strcpy(cup[1],cup[3]);
-                        strcpy(token,cup[1]);
-                    }
-                    else if(strcmp(word[j],"D")){
-                        strcpy(cup[4],token);
-                        strcpy(cup[1],cup[4]);
-                        strcpy(token,cup[1]);
-                        strcpy(cup[3],token);
-                        strcpy(cup[2],cup[3]);
-                        strcpy(token,cup[2]);
-                    }
-                }
-                for( j=0 ; j<4 ; j++ ){
-                    if(strcmp(cup[j],"A")){
-                        printf("%d",j+1);
-                    }
-                }
+
             }
 
         }
