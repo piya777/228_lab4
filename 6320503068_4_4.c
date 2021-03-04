0d5e0b231987df0a2d7b959fd4f18f57a7e579c0
Piya <piya.rat@ku.th>
2021-03-04 16:57:51 +0700

    9

--- 6320503068_4_4.c
+++ 6320503068_4_4.c
@@ -4,19 +4,20 @@
         char word[1000],no[11][5]={"i","of","the","on","at","for","with","a","an","in","and"};
         int i,check=0;
         scanf("%s",word);
-        char* use=strtok(word," ");
+        char *use=strtok(word," ");
+        use=strtok(word," ");
         if(strlen(word)<=1000){
             while(use!=NULL){
-                for(i=0 ; i<12 ; i++){
-                    if(!strcmp(use,no[i])){
+                for(i=0;i<11;i++)
+                    if(strcmp(use,no[i])){
                         check++;
                     }
-                }
-                if(check==11){
+                if(check==11)
+                {
                     printf("%c",toupper(use[0]));
                 }
-                i=1;
                 use=strtok(NULL," ");
+                check=0;
             }
         }
 
