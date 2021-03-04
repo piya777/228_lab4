89c4f7c6e46be213ba67e8b737545e186c926f23
Piya <piya.rat@ku.th>
2021-03-04 17:19:53 +0700

    12

--- 6320503068_4_4.c
+++ 6320503068_4_4.c
@@ -9,10 +9,17 @@
         use=strtok(word," ");
         while(use!=NULL)
         {
-            if(strcmp(use,"i")!=0&&strcmp(use,"of")!=0&&strcmp(use,"the")!=0&&strcmp(use,"on")!=0&&strcmp(use,"at")!=0
-               &&strcmp(use,"for")!=0&&strcmp(use,"with")!=0&&strcmp(use,"a")!=0&&strcmp(use,"an")!=0&&strcmp(use,"in")!=0
-               &&strcmp(use,"and")!=0){
+            if(i==0)
+            {
                 printf("%c",toupper(use[0]));
+            }
+            else
+            {
+                if(strcmp(use,"i")!=0&&strcmp(use,"of")!=0&&strcmp(use,"the")!=0&&strcmp(use,"on")!=0&&strcmp(use,"at")!=0
+                &&strcmp(use,"for")!=0&&strcmp(use,"with")!=0&&strcmp(use,"a")!=0&&strcmp(use,"an")!=0&&strcmp(use,"in")!=0
+                &&strcmp(use,"and")!=0){
+                    printf("%c",toupper(use[0]));
+                }
             }
             use=strtok(NULL," ");
             if(i==0){
