a4dcfbaa2f0d602c810008b9977b7933619dcec5
Piya <piya.rat@ku.th>
2021-03-04 17:16:14 +0700

    11

--- 6320503068_4_4.c
+++ 6320503068_4_4.c
@@ -1,25 +1,23 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
     void main(){
-        char word[1000] ,*use ,no[11][5]={"i","of","the","on","at","for","with","a","an","in","and"};
-        int i,j,check=0;
-        gets(word);
+        char word[1000];
+        char *use;
+        int i=0,j,check=0;
+        scanf("%[^\n]s",word);
         use=strtok(word," ");
         while(use!=NULL)
         {
-            for(i=0;i<11;i++){
-                if(strcmp(use,no[i])){
-                    check++;
-                }
-            }
-            if(j==0){
-                printf("%c",toupper(use[0]));
-            }
-            if(check==11){
+            if(strcmp(use,"i")!=0&&strcmp(use,"of")!=0&&strcmp(use,"the")!=0&&strcmp(use,"on")!=0&&strcmp(use,"at")!=0
+               &&strcmp(use,"for")!=0&&strcmp(use,"with")!=0&&strcmp(use,"a")!=0&&strcmp(use,"an")!=0&&strcmp(use,"in")!=0
+               &&strcmp(use,"and")!=0){
                 printf("%c",toupper(use[0]));
             }
             use=strtok(NULL," ");
-            j++,check=0;
+            if(i==0){
+                i++;
+            }
         }
 
     }
